84e6362bbd3c3d1faf28cfe09198ec8c1f7bfec4
imanmansour86 <imanmansour86@gmail.com>
2022-03-06 21:00:59 -0800

    add get product by id route

--- server/controllers/api/productRoutes.js
+++ server/controllers/api/productRoutes.js
@@ -4,7 +4,18 @@ const { Product } = require("../../models");
 //get all products
 router.get("/", async (req, res) => {
   try {
-    const productData = await Product.findAll({});
+    const products = await Product.findAll({});
+    res.json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    console.log("hhh", req.params.id);
+    const productData = await Product.findByPk(req.params.id);
+
     res.json(productData);
   } catch (err) {
     res.status(500).json(err);
