d43bc17707bd1b355e665375b46eb9101d39991d
imanmansour86 <imanmansour86@gmail.com>
2022-03-10 15:44:00 -0800

    styling checkout page

--- client/src/components/pages/Cart/Cart.js
+++ client/src/components/pages/Cart/Cart.js
@@ -3,73 +3,162 @@ import "./style.css";
 
 const Cart = () => {
   return (
-    <div>
-      <h1>Your Cart</h1>
-
-      <div className="cart">
-        <div className="container-fluid">
-          <div className="row align-items-start">
-            <div className="col-12 col-sm-8 items">
-              <div className="cartItem row align-items-start">
-                <div className="col-3 mb-2">
-                  <img
-                    className="w-100"
-                    src="https://badux.co/smc/codepen/birdcage-posters.jpg"
-                    alt="art image"
-                  />
+    <div className="container mt-5 mb-5">
+      <div className="d-flex justify-content-center row">
+        <div className="cart-page col-md-8">
+          <div className="p-2"></div>
+          <div className="d-flex flex-row justify-content-between align-items-center p-2 bg-white mt-4 px-3 rounded">
+            <div className="mr-1">
+              <img
+                class="rounded"
+                src="https://i.imgur.com/XiFJkhI.jpg"
+                width="70"
+              />
+            </div>
+            <div className="d-flex flex-column align-items-center product-details">
+              <span className="font-weight-bold">Product Name</span>
+              <div className="d-flex flex-row product-desc">
+                <div className="size mr-1">
+                  <span className="text-grey">Size:</span>
+                  <span className="font-weight-bold">&nbsp;M</span>
                 </div>
-                <div className="col-5 mb-2">
-                  <p className="pl-1 mb-0">Product Name</p>
-                </div>
-                <div className="col-2">
-                  <p className="cartItemQuantity p-1 text-center">1</p>
-                </div>
-                <div className="col-2">
-                  <p id="cartItem1Price">$66</p>
+                <div className="color">
+                  <span className="text-grey">Color:</span>
+                  <span className="font-weight-bold">&nbsp;Grey</span>
                 </div>
               </div>
-              <hr />
-
-              <hr />
             </div>
-            <div className="col-12 col-sm-8 items">
-              <div className="cartItem row align-items-start">
-                <div className="col-3 mb-2">
-                  <img
-                    className="w-100"
-                    src="https://badux.co/smc/codepen/birdcage-posters.jpg"
-                    alt="art image"
-                  />
+            <div className="d-flex flex-row align-items-center qty">
+              <i className="fa fa-minus text-danger"></i>
+              <h5 className="text-grey mt-1 mr-1 ml-1">2</h5>
+              <i className="fa fa-plus text-success"></i>
+            </div>
+            <div>
+              <h5 className="text-grey">$20.00</h5>
+            </div>
+            <div className="d-flex align-items-center">
+              <i className="fa fa-trash mb-1 text-danger"></i>
+            </div>
+          </div>
+          <div className="d-flex flex-row justify-content-between align-items-center p-2 bg-white mt-4 px-3 rounded">
+            <div className="mr-1">
+              <img
+                className="rounded"
+                src="https://i.imgur.com/XiFJkhI.jpg"
+                width="70"
+              />
+            </div>
+            <div className="d-flex flex-column align-items-center product-details">
+              <span className="font-weight-bold">Basic T-shirt</span>
+              <div className="d-flex flex-row product-desc">
+                <div className="size mr-1">
+                  <span className="text-grey">Size:</span>
+                  <span className="font-weight-bold">&nbsp;M</span>
                 </div>
-                <div className="col-5 mb-2">
-                  <p className="pl-1 mb-0">Product Name</p>
-                </div>
-                <div className="col-2">
-                  <p className="cartItemQuantity p-1 text-center">1</p>
-                </div>
-                <div className="col-2">
-                  <p id="cartItem1Price">$66</p>
+                <div className="color">
+                  <span className="text-grey">Color:</span>
+                  <span className="font-weight-bold">&nbsp;Grey</span>
                 </div>
               </div>
-              <hr />
-
-              <hr />
             </div>
-            <div className="col-12 col-sm-4 p-3 proceed form">
-              <div className="row mx-0 mb-2">
-                <div className="col-sm-8 p-0 d-inline">
-                  <h5>Total</h5>
+            <div className="d-flex flex-row align-items-center qty">
+              <i className="fa fa-minus text-danger"></i>
+              <h5 className="text-grey mt-1 mr-1 ml-1">2</h5>
+              <i className="fa fa-plus text-success"></i>
+            </div>
+            <div>
+              <h5 className="text-grey">$20.00</h5>
+            </div>
+            <div className="d-flex align-items-center">
+              <i className="fa fa-trash mb-1 text-danger"></i>
+            </div>
+          </div>
+          <div className="d-flex flex-row justify-content-between align-items-center p-2 bg-white mt-4 px-3 rounded">
+            <div className="mr-1">
+              <img
+                className="rounded"
+                src="https://i.imgur.com/XiFJkhI.jpg"
+                width="70"
+              />
+            </div>
+            <div className="d-flex flex-column align-items-center product-details">
+              <span className="font-weight-bold">Basic T-shirt</span>
+              <div className="d-flex flex-row product-desc">
+                <div className="size mr-1">
+                  <span className="text-grey">Size:</span>
+                  <span className="font-weight-bold">&nbsp;M</span>
                 </div>
-                <div className="col-sm-4 p-0">
-                  <p id="total">$138.40</p>
+                <div className="color">
+                  <span className="text-grey">Color:</span>
+                  <span className="font-weight-bold">&nbsp;Grey</span>
                 </div>
               </div>
-              <a href="#">
-                <button id="btn-checkout" class="shopnow">
-                  <span>Checkout</span>
-                </button>
-              </a>
             </div>
+            <div className="d-flex flex-row align-items-center qty">
+              <i className="fa fa-minus text-danger"></i>
+              <h5 className="text-grey mt-1 mr-1 ml-1">2</h5>
+              <i className="fa fa-plus text-success"></i>
+            </div>
+            <div>
+              <h5 className="text-grey">$20.00</h5>
+            </div>
+            <div className="d-flex align-items-center">
+              <i className="fa fa-trash mb-1 text-danger"></i>
+            </div>
+          </div>
+          <div className="d-flex flex-row justify-content-between align-items-center p-2 bg-white mt-4 px-3 rounded">
+            <div className="mr-1">
+              <img
+                class="rounded"
+                src="https://i.imgur.com/XiFJkhI.jpg"
+                width="70"
+              />
+            </div>
+            <div className="d-flex flex-column align-items-center product-details">
+              <span class="font-weight-bold">Basic T-shirt</span>
+              <div className="d-flex flex-row product-desc">
+                <div className="size mr-1">
+                  <span className="text-grey">Size:</span>
+                  <span className="font-weight-bold">&nbsp;M</span>
+                </div>
+                <div className="color">
+                  <span className="text-grey">Color:</span>
+                  <span className="font-weight-bold">&nbsp;Grey</span>
+                </div>
+              </div>
+            </div>
+            <div className="d-flex flex-row align-items-center qty">
+              <i className="fa fa-minus text-danger"></i>
+              <h5 className="text-grey mt-1 mr-1 ml-1">2</h5>
+              <i className="fa fa-plus text-success"></i>
+            </div>
+            <div>
+              <h5 className="text-grey">$20.00</h5>
+            </div>
+            <div className="d-flex align-items-center">
+              <i className="fa fa-trash mb-1 text-danger"></i>
+            </div>
+          </div>
+          <div className="d-flex flex-row align-items-center mt-3 p-2 bg-white rounded">
+            <input
+              type="text"
+              className="form-control border-0 gift-card"
+              placeholder="discount code/gift card"
+            />
+            <button
+              className="btn btn-outline-warning btn-sm ml-2"
+              type="button"
+            >
+              Apply
+            </button>
+          </div>
+          <div className="d-flex flex-row align-items-center mt-3 p-2 bg-white rounded">
+            <button
+              className="btn btn-warning btn-block btn-lg ml-2 pay-button"
+              type="button"
+            >
+              Proceed Checkout
+            </button>
           </div>
         </div>
       </div>
