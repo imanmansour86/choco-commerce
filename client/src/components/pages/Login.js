6fbe2a2dff603d43ab2c4f871a8d65d15d6c24c2
imanmansour86 <imanmansour86@gmail.com>
2022-03-09 21:42:05 -0800

    styling login page

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Login = () => {
-  return <div>login</div>;
+  return (
+    <div className="form-body">
+      <h1>Login</h1>
+      <form className="sign-in">
+        <div className="row">
+          <label for="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            autocomplete="off"
+            placeholder="email@example.com"
+          />
+        </div>
+        <div className="row">
+          <label for="password">Password</label>
+          <input type="password" name="password" />
+        </div>
+        <button className="sign" type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
