1a4cfe3c4335267e5a6db217f71d546d615f7ec9
imanmansour86 <imanmansour86@gmail.com>
2022-03-13 22:02:37 -0700

    adding returned withsucss state in cart

--- client/src/components/pages/Cart/Cart.js
+++ client/src/components/pages/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useStoreContext } from "../../../utils/GlobalState";
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../../utils/actions";
 import "./style.css";
@@ -14,6 +14,8 @@ import { Link } from "react-router-dom";
 
 const Cart = () => {
   const [{ cartItems }, dispatch] = useStoreContext();
+  const [returnedWithSuccess, setReturnedWithSuccess] = useState();
+  const [returnedWithFailure, setReturnedWithFailure] = useState();
 
   const removeFromCart = (product) => {
     console.log("btn clicked", product);
@@ -31,74 +33,88 @@ const Cart = () => {
     });
   };
 
-  console.log("state", cartItems);
+  const onSuccess = (data) => {
+    //post api
+    setReturnedWithSuccess(data);
+  };
+  const onFailure = (data) => {
+    setReturnedWithFailure(data);
+  };
+
   return (
     <div className="container mt-5 mb-5">
       <div className="d-flex justify-content-center row">
-        <div className="cart-page col-md-8">
-          <div className="p-2"></div>
+        {returnedWithSuccess ? (
+          <div>{JSON.stringify(returnedWithSuccess)}</div>
+        ) : (
+          <div className="cart-page col-md-8">
+            <div className="p-2"></div>
 
-          {cartItems.map((item) => (
-            <div
-              key={item.product.id}
-              className="d-flex flex-row justify-content-between align-items-center p-2 bg-white mt-4 px-3 rounded"
-            >
-              <div className="mr-1">
-                <img
-                  className="rounded"
-                  src="https://i.imgur.com/XiFJkhI.jpg"
-                  width="70"
-                />
-              </div>
-              <div className="d-flex flex-column align-items-center ">
-                <span className=" text-blue">{item.product.name}</span>
-              </div>
-              <div className="d-flex flex-row align-items-center qty">
-                <input
-                  placeholder="1"
-                  type="number"
-                  id="quantity"
-                  name="quantity"
-                  min="1"
-                  max="9"
-                  value={item.purchaseQuantity}
-                  onChange={(e) => onChange(e.target.value, item.product)}
-                />
-                {/* <i className="fa fa-minus text-danger"></i>
+            {returnedWithFailure && <div>Your payment failed</div>}
+            {cartItems.map((item) => (
+              <div
+                key={item.product.id}
+                className="d-flex flex-row justify-content-between align-items-center p-2 bg-white mt-4 px-3 rounded"
+              >
+                <div className="mr-1">
+                  <img
+                    className="rounded"
+                    src="https://i.imgur.com/XiFJkhI.jpg"
+                    width="70"
+                  />
+                </div>
+                <div className="d-flex flex-column align-items-center ">
+                  <span className=" text-blue">{item.product.name}</span>
+                </div>
+                <div className="d-flex flex-row align-items-center qty">
+                  <input
+                    placeholder="1"
+                    type="number"
+                    id="quantity"
+                    name="quantity"
+                    min="1"
+                    max="9"
+                    value={item.purchaseQuantity}
+                    onChange={(e) => onChange(e.target.value, item.product)}
+                  />
+                  {/* <i className="fa fa-minus text-danger"></i>
                 <h5 className="text-grey mt-1 mr-1 ml-1">2</h5>
                 <i className="fa fa-plus text-success"></i> */}
+                </div>
+                <div>
+                  <h5 className="text-blue">{item.product.price}</h5>
+                </div>
+                <div className="d-flex align-items-center">
+                  <i
+                    onClick={() => removeFromCart(item.product)}
+                    className="fa fa-trash mb-1 text-danger"
+                  ></i>
+                </div>
               </div>
-              <div>
-                <h5 className="text-blue">{item.product.price}</h5>
-              </div>
-              <div className="d-flex align-items-center">
-                <i
-                  onClick={() => removeFromCart(item.product)}
-                  className="fa fa-trash mb-1 text-danger"
-                ></i>
-              </div>
+            ))}
+
+            <div className="d-flex flex-row align-items-center mt-3 p-2 bg-white rounded">
+              <div className="text-grey">Order Total: 20$</div>
             </div>
-          ))}
+            <div className="checkbtn d-flex flex-row align-items-center mt-3 p-2 bg-white rounded">
+              <button
+                className="btn btn-warning btn-block btn-lg ml-2 "
+                type="button"
+              >
+                <CheckoutForm
+                  name={"LaRoche Chocolate"}
+                  description={"Current Balance"}
+                  amount={4.99}
+                  shippingAddress={"3375 Payne Drive"}
+                  onSuccess={onSuccess}
+                  onFailure={onFailure}
+                />
 
-          <div className="d-flex flex-row align-items-center mt-3 p-2 bg-white rounded">
-            <div className="text-grey">Order Total: 20$</div>
+                {/* Proceed Checkout */}
+              </button>
+            </div>
           </div>
-          <div className="checkbtn d-flex flex-row align-items-center mt-3 p-2 bg-white rounded">
-            <button
-              className="btn btn-warning btn-block btn-lg ml-2 "
-              type="button"
-            >
-              <CheckoutForm
-                name={"LaRoche Chocolate"}
-                description={"Current Balance"}
-                amount={4.99}
-                shippingAddress={"3375 Payne Drive"}
-              />
-
-              {/* Proceed Checkout */}
-            </button>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
--- client/src/components/pages/CheckoutForm.js
+++ client/src/components/pages/CheckoutForm.js
@@ -15,43 +15,47 @@ const CURRENCY = "USD";
 
 const fromDollarToCent = (amount) => parseInt(amount * 100);
 
-const successPayment = (data) => {
-  alert("Payment Successful");
+const CheckoutForm = ({ name, description, amount, onSuccess, onFailure }) => {
+  const successPayment = (data) => {
+    console.log(data);
+    onSuccess(data);
+  };
+
+  const errorPayment = (data) => {
+    console.error(data);
+    onFailure(data);
+  };
+
+  const onToken = (amount, description) => (token) =>
+    axios
+      .post(PAYMENT_SERVER_URL, {
+        description,
+        source: token.id,
+        currency: CURRENCY,
+        amount: 100,
+      })
+      .then(successPayment)
+      .catch(errorPayment);
+  return (
+    <StripeCheckout
+      name={name}
+      description={description}
+      amount={fromDollarToCent(amount)}
+      token={onToken(amount, description)}
+      currency={CURRENCY}
+      stripeKey={
+        "pk_test_51KbALaFc4X4KsU22jdX47Ta9QxT1GUQGJhzzsypQbExkjRSUDyf8c9F4CaEMj741evosIEXTzEr3lAO2Ib425vsb00DACZuj78"
+      }
+      zipCode
+      email
+      allowRememberMe
+      shippingAddress
+    >
+      <button className="btn btn-warning btn-block btn-lg ml-2">
+        Proceed to Checkout
+      </button>
+    </StripeCheckout>
+  );
 };
-
-const errorPayment = (data) => {
-  console.error(data);
-  alert("Payment Error");
-};
-
-const onToken = (amount, description) => (token) =>
-  axios
-    .post(PAYMENT_SERVER_URL, {
-      description,
-      source: token.id,
-      currency: CURRENCY,
-      amount: 100,
-    })
-    .then(successPayment)
-    .catch(errorPayment);
-
-const CheckoutForm = ({ name, description, amount, shippingAddress }) => (
-  <StripeCheckout
-    name={name}
-    description={description}
-    amount={fromDollarToCent(amount)}
-    token={onToken(amount, description)}
-    currency={CURRENCY}
-    stripeKey={
-      "pk_test_51KbALaFc4X4KsU22jdX47Ta9QxT1GUQGJhzzsypQbExkjRSUDyf8c9F4CaEMj741evosIEXTzEr3lAO2Ib425vsb00DACZuj78"
-    }
-    zipCode
-    email
-    allowRememberMe
-    shippingAddress
-  >
-    <button className="btn btn-primary">Proceed to Checkout</button>
-  </StripeCheckout>
-);
 
 export default CheckoutForm;
