eaf3116f51993d1eb04cc734a19347b058003d71
imanmansour86 <imanmansour86@gmail.com>
2022-03-14 14:08:28 -0700

    adding images to seed data

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { ADD_TO_CART } from "../utils/actions";
 import { useStoreContext } from "../utils/GlobalState";
-
+import "../assets/nougat.jpeg";
 const Home = () => {
   const [products, setProducts] = useState([]);
   const [error, setError] = useState(null);
@@ -44,17 +44,13 @@ const Home = () => {
         <div className="all-items">
           {products.map((product) => (
             <div key={product.id} className="card">
-              <img
-                className="card-img-top"
-                src="https://media.istockphoto.com/photos/chocolate-assortment-background-top-view-of-different-kinds-of-picture-id1276645382?b=1&k=20&m=1276645382&s=170667a&w=0&h=ba4ZvgTsuvpIqTzAlYLXVqbcpxVeoKh2cKOUgYoaJN8="
-                alt="Card image cap"
-              />
+              <img src={require(`../assets/${product.image}`)} />
               <div className="card-body">
                 <Link to={`/products/${product.id}`}>
                   <h5 className="card-title">{product.name}</h5>
                 </Link>
                 <p className="card-text">
-                  Price: <em>$449</em>
+                  Price: <em>${product.price}</em>
                 </p>
                 <a
                   onClick={() => addToCart(product)}
--- server/seeds/productData.js
+++ server/seeds/productData.js
@@ -5,6 +5,7 @@ const productData = [
     name: "Milk Nougat",
     description:
       "Brown sugar nougat with roasted, unsalted peanuts in smooth milk chocolate",
+    image: "nougat.jpeg",
     price: 10,
   },
 
@@ -12,6 +13,7 @@ const productData = [
     name: "Dark Vanilla Buttercream",
     description:
       "Buttery, creamy soft center with a touch of vanilla, enrobed in rich dark chocolate",
+    image: "darkbuttercream.jpeg",
     price: 30,
   },
 
@@ -19,12 +21,14 @@ const productData = [
     name: "Dark caramel",
     description:
       "Chocolate butter caramel and creamy vanilla caramel coated in smooth dark chocolate",
+    image: "dark.jpeg",
     price: 40,
   },
   {
     name: "Milk caramel",
     description:
       "Chocolate butter caramel and creamy vanilla caramel coated in smooth milk chocolate",
+    image: "milk.jpeg",
     price: 20,
   },
 
@@ -32,6 +36,7 @@ const productData = [
     name: "Raspberry Truffle",
     description:
       "Truffle center of dark chocolate, cream and raspberries covered in rich dark chocolate",
+    image: "raspberry.jpeg",
     price: 30,
   },
 
@@ -39,32 +44,37 @@ const productData = [
     name: "Milk Coconut Cream",
     description:
       "Creamy soft center with angel flake coconut covered in smooth milk chocolate",
+    image: "coconut.jpeg",
     price: 40,
   },
   {
     name: "Dark Chocolate Truffle",
     description:
       "Rich, creamy truffle center of semi-sweet chocolate and chocolate chips covered in rich dark chocolate",
+    image: "dark-chocolate-truffle.jpeg",
     price: 20,
   },
 
   {
-    name: "Milk Almond Toffee",
+    name: "Milk Mocha",
     description:
-      " Chopped almonds with crunchy toffee smothered in our signature milk chocolate",
+      " Creamy center flavored with coffee and cream covered in smooth milk chocolate and decorated with chocolate sprinkles",
+    image: "mocha.jpeg",
     price: 30,
   },
 
   {
-    name: "Milk Walnut Square",
+    name: "Milk Walnut",
     description:
-      "Walnuts combined with buttery caramel and dipped halfway in rich dark chocolate",
+      "Walnuts combined with buttery caramel and dipped halfway in rich milk chocolate",
+    image: "milk-walnut.jpeg",
     price: 40,
   },
   {
     name: "Milk Caramel Almond",
     description:
       "Buttery brown sugar caramel with a touch of maple sugar and almonds enrobed in smooth milk chocolate",
+    image: "almond-square.jpeg",
     price: 20,
   },
 
@@ -72,6 +82,7 @@ const productData = [
     name: "Milk Vanilla Buttercream",
     description:
       "Buttery, creamy soft center with a touch of vanilla, covered in smooth milk chocolate.",
+    image: "milk-buttercream.jpeg",
     price: 30,
   },
 
@@ -79,12 +90,14 @@ const productData = [
     name: "Dark caramel",
     description:
       "Chocolate butter caramel and creamy vanilla caramel coated in smooth dark chocolate",
+    image: "dark-pattie.jpeg",
     price: 40,
   },
   {
     name: "Dark Peanut Brittle",
     description:
       "Buttery peanut brittle wrapped in rich, creamy dark chocolate",
+    image: "dark-peanut-brittle.jpeg",
     price: 20,
   },
 
@@ -92,6 +105,7 @@ const productData = [
     name: "Lemon Truffle",
     description:
       "Smooth truffle center of white chocolate and lemon covered in dark chocolate",
+    image: "lemon-truffle.jpeg",
     price: 30,
   },
 
@@ -99,12 +113,14 @@ const productData = [
     name: "Apple Pie Truffle",
     description:
       "Creamy truffle center of white chocolate, Granny Smith Apples and a touch of cinnamon covered in white chocolat",
+    image: "apple-pie-truffle.jpeg",
     price: 40,
   },
   {
     name: "Dark Marzipan",
     description:
       "Almond paste with honey and powdered sugar covered in rich dark chocolate.",
+    image: "marzipan.jpeg",
     price: 40,
   },
 ];
