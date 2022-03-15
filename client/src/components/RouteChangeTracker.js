5c7695f815e25df0391d1f17537e19b25dbfe6c2
imanmansour86 <imanmansour86@gmail.com>
2022-03-14 22:55:25 -0700

    using use location instead of react router

--- client/src/components/RouteChangeTracker.js
+++ client/src/components/RouteChangeTracker.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { withRouter } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+
 import ReactGA from "react-ga";
 const RouteChangeTracker = ({ history }) => {
-  history.listen((location, action) => {
-    ReactGA.set({ page: location.pathname });
-    ReactGA.pageview(location.pathname);
-  });
+  let location = useLocation();
+
+  React.useEffect(() => {
+    ReactGA("send", "pageview");
+  }, [location]);
 
   return <div></div>;
 };
 
-export default withRouter(RouteChangeTracker);
+export default RouteChangeTracker;
