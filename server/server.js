d5793fb08d61a349db09ee8778926e8141619049
imanmansour86 <imanmansour86@gmail.com>
2022-03-14 21:41:55 -0700

    fixing loading page on heroku

--- server/server.js
+++ server/server.js
@@ -24,7 +24,10 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 app.use(routes);
 // configureRoutes(app);
