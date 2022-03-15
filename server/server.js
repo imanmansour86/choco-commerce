18feed1421af576705628141d9f535fc4b6010a2
imanmansour86 <imanmansour86@gmail.com>
2022-03-14 21:43:57 -0700

    adding access to build files

--- server/server.js
+++ server/server.js
@@ -25,6 +25,10 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
